aa89c555ef813dee41626844ecfa6d5ac5244820
aryan <ac6430873@gmail.com>
2026-08-07 17:23:45 +0530

    loops

---
+++ scripts/11-todo.js
@@ -0,0 +1,13 @@
+const todoList =[];
+function addTodo() {
+  const inputElement = document.querySelector('.js-todo');
+    const name = inputElement.value;
+  todoList.push(name); 
+  console.log(todoList);
+  }
+
+
+
+
+   
+  
