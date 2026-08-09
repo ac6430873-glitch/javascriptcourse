44bebafb3add8a0429b6a3f8ef5773e2ef9131d0
aryan <ac6430873@gmail.com>
2026-08-09 11:34:37 +0530

    todo-list

--- scripts/11-todo.js
+++ scripts/11-todo.js
@@ -1,11 +1,42 @@
-const todoList =[];
-function addTodo() {
-  const inputElement = document.querySelector('.js-todo');
-    const name = inputElement.value;
-  todoList.push(name); 
-  console.log(todoList);
+const todoList = [{name:'make dinner', dueDate:''}, {name:'wash dishes', dueDate:''}];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    const name = todoObject.name;
+    const dueDate = todoObject.dueDate;
+    const HTML = `
+    <div>
+      ${name}</div>
+     <div>(${dueDate})</div>
+    <button onclick="
+    todoList.splice(${i}, 1);
+    renderTodoList();
+    ">Delete</button>
+    `;
+    todoListHTML += HTML;
   }
 
+  document.querySelector('.js-a').innerHTML = todoListHTML;
+}
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-todo');
+  const name = inputElement.value;
+
+  const dateInput = document.querySelector('.js-due-date');
+const dueDate = dateInput.value;
+  todoList.push({name:name, 
+    dueDate:dueDate});
+  
+
+  inputElement.value = '';
+  renderTodoList();
+}
 
 
 
